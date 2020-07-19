3ba3ef22754ee52d1c3fda5d1673f1570a6925c3
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2020-07-19 20:50:42 -0300

    Personalização

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,10 @@
   <div id="app">
     <div class="ui inverted vertical masthead center aligned segment">
       <div class="ui text container">
-        <h1 class="ui inverted header"> Aplicação Todo List com Vue.js 3</h1>
-        <h2 class="ui inverted header">Glaucia Lemos
-          <div class="sub header">Cloud Advocate em JavaScript/Node.js @ Microsoft</div>
+        <h1 class="ui inverted header">Cópia da Aplicação Todo List com Vue.js 3 by Glaucia Lemos</h1>
+        <h2 class="ui inverted header">Thyago Vinicius
+          <div class="sub header">Developer</div>
         </h2>
-        <h3 class="ui inverted header">Rio de Janeiro, Brasil - 2020</h3>
-        <a class="tiny ui youtube button" type="button" href="https://www.youtube.com/user/l32759">
-          <i class="youtube icon"></i>
-          YouTube
-        </a>
-        <a class="tiny ui linkedin button" type="button" href="https://www.linkedin.com/in/glaucialemos/">
-          <i class="linkedin icon"></i>
-          LinkedIn
-        </a>
-        <a class="tiny ui twitter button" type="button" href="https://twitter.com/glaucia_lemos86">
-          <i class="twitter icon"></i>
-          Twitter
-        </a>
       </div>
     </div>
     <br />
